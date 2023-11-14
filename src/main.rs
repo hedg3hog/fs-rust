2a7f9610c4c0e625990be73ae8bbd1e8c59d6c07
Rouven Bissinger <contact@rouvenbissinger.com>
2023-11-14 11:04:17 +0100

    add firat function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+use std::fs::read_to_string;
+use std::time::Instant;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
+
+#[derive(Serialize, Deserialize,PartialEq, Clone, )]
+struct Bid {
+    items: HashSet<u16>,
+    value: i32
+}
+
+fn load_bids(path: &str) -> Vec<Bid>{
+    let f = read_to_string(path).unwrap();
+    let bids : Vec<Bid> = serde_json::from_str(&f).unwrap();
+    return bids
+}
 
 fn main() {
     println!("Hello, world!");
