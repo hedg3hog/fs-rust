d0c5a828976951f5fdbf93d6ac9f064268c6966e
Rouven Bissinger <contact@rouvenbissinger.com>
2023-11-16 13:59:30 +0100

    make it faster

--- src/main.rs
+++ src/main.rs
@@ -16,36 +16,37 @@ fn load_bids(path: &str) -> Vec<Bid>{
     return bids
 }
 
-fn bid_sum(bids : Vec<Bid>) -> u32 {
-    bids.iter().map(|b| b.value).sum()
+fn bid_sum(bids_idx: &[usize], bids : &[Bid]) -> u32 {
+    bids_idx.iter().map(|&b| bids[b].value).sum()
 }
 
-fn prune_bids(path: Vec<Bid>, list_to_check: Vec<Bid>) -> Vec<Bid> {
-    let sold: HashSet<u16> = path.iter().flat_map(|b| b.items.iter()).cloned().collect();
-    list_to_check.into_iter().filter(|b| sold.is_disjoint(&b.items)).collect()
+fn prune_bids(path: &[usize], list_to_check: Vec<usize>, bids: &[Bid]) -> Vec<usize> {
+    let sold: HashSet<u16> = path.iter().flat_map(|&b| bids[b].items.iter().cloned()).collect();
+    list_to_check.into_iter().filter(|&b| sold.is_disjoint(&bids[b].items)).collect()
 }
 
-fn full_search(bids : Vec<Bid>, best_value: u32, best_path: Vec<Bid>, current_path: Vec<Bid>) -> (Vec<Bid>, u32){
-    if bids.len() == 0 {
+
+fn full_search(bids_idx : Vec<usize>, best_value: u32, best_path: Vec<usize>, current_path: Vec<usize>, bids: &Vec<Bid>) -> (Vec<usize>, u32){
+    if bids_idx.len() == 0 {
         return (best_path, best_value)
     }
     let mut best_value = best_value.clone();
     let mut best_path = best_path;
-    for b in bids.clone(){
+    for b in bids_idx.clone(){
         // break if best value can not be over bidden
-        if bid_sum(bids.clone()) + bid_sum(current_path.clone()) < best_value {
+        if bid_sum(&bids_idx, &bids) + bid_sum(&current_path, &bids) < best_value {
             break;
         }
         let mut c_path = current_path.clone();
         c_path.push(b.clone());
 
-        let x =  bid_sum(c_path.clone());
+        let x =  bid_sum(&c_path, &bids);
         if  x > best_value {
             best_value = x;
             best_path = c_path.clone();
         }
 
-    (best_path, best_value) = full_search(prune_bids(c_path.clone(), bids.clone()), best_value, best_path, c_path)
+    (best_path, best_value) = full_search(prune_bids(&c_path, bids_idx.clone(), &bids), best_value, best_path, c_path, bids)
 
         
 }
@@ -58,15 +59,17 @@ fn full_search(bids : Vec<Bid>, best_value: u32, best_path: Vec<Bid>, current_pa
 
 }
 fn main() {
+    let n: usize = 28;
     let bids_ = load_bids("bids03-ID.json");
-    let selection = &bids_[..28];
+    let selection = &bids_[..n];
+    let selection = selection.to_vec();
     println!("Bids to check: {}", selection.len());
     let now = Instant::now();
-    let (winner, value) = full_search(selection.to_vec(), 0, vec![], vec![]);
+    let (winner, value) = full_search((0..n).collect(), 0, vec![], vec![], &selection);
     println!("Time: {}", now.elapsed().as_secs_f32());
     println!("value: {}", value);
     println!("winner len {}", winner.len());
-    println!("winner sum {}", bid_sum(winner.clone()));
+    println!("winner sum {}", bid_sum(&winner.clone(), &selection));
 
 
 
